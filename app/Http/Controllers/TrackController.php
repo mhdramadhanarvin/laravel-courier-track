00ddeca30d1b49089336790e6307011bbcfeda75
Ubuntu <ubuntu@ip-172-31-37-33.ap-southeast-1.compute.internal>
2022-05-17 16:25:21 +0000

    track page

--- app/Http/Controllers/TrackController.php
+++ app/Http/Controllers/TrackController.php
@@ -6,8 +6,8 @@ use Illuminate\Http\Request;
 
 class TrackController extends Controller
 {
-    public function track() 
+    public function track(Request $request) 
     {
-        
+        dd($request->all());
     }
 }
